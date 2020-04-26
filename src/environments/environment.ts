764e2ed0c3c7fec1f29fd0098f442efd189b159b
unknown <key.pc99@hotmail.com>
2020-04-26 18:01:10 -0500

    initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBdK59Mf6oKekNJCtA-x3R-e47dbgi-6Mw",
+    authDomain: "angular-crud-firebase-97d9c.firebaseapp.com",
+    databaseURL: "https://angular-crud-firebase-97d9c.firebaseio.com",
+    projectId: "angular-crud-firebase-97d9c",
+    storageBucket: "angular-crud-firebase-97d9c.appspot.com",
+    messagingSenderId: "1097452694887",
+    appId: "1:1097452694887:web:a39d274e6dfc2dccc753c2",
+    measurementId: "G-DDJDPEFB47"
+  }
+};
+
